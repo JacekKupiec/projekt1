c9eed5ef83f72fdda0fa248dc631ea95b025dd33
JacekKupiec <jacek.kupiec@o2.pl>
2014-07-07 12:08:00 +0200

    Duża rozbudowa, dziala wyświetlanie artykulow i przechowywanie ich'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,26 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    #tak nie mzna bo musimy określić dokładnie jakie parametry przesylamy.
+    #@artile = Article.new(params[:article])
+    #@article.save
+    #redirec_to @article
+
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find params[:id]
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end

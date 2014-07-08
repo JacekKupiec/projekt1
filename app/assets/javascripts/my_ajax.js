6e6d691b0a38515902357c9a843eca3c79668017
JacekKupiec <jacek.kupiec@o2.pl>
2014-07-08 12:05:55 +0200

    jQuery praktyka

--- app/assets/javascripts/my_ajax.js
+++ app/assets/javascripts/my_ajax.js
@@ -3,20 +3,6 @@
  */
 
 $(document).ready(function (){
-    /*$(document).ajaxSuccess(function () {
-        console.log('Item was deleted');
-    })
-
-    $('a.del').click(function() {
-        console.log('test');
-    })*/
-
-    /*$(document).on("ajax:success", "a.show", function (){
-       alert($(this).parent('td').sibling(2,'td').text());
-    });*/
-
-
-
    $(document).on("click", "a.del",  function (){
        $(this).parent().parent().remove();
    })
@@ -24,10 +10,13 @@ $(document).ready(function (){
       var href_val = $(this).attr('href').split('/');
 
        $.get('/articles/' + $(this).attr('href').split('/')[href_val.length - 1], function (dane){
-           alert('otrzymano dane: '+ dane['status'] + "\nTytuł: " + dane['title'] + "\nTekst: " + dane['text']);
-           $('body').append(('<div id="Komunikat">').css('background-color', 'red').
-               append('otrzymano dane: '+ dane['status'] + "<br/>Tytuł: " + dane['title'] + "<br/>Tekst: " + dane['text']).
-               show(3000));
+           if ($('#Komunikat').length > 0)
+            $('#Komunikat').empty().append('otrzymano dane: '+ dane['status'] + "<br/>Tytuł: " + dane['title'] + "<br/>Tekst: " + dane['text']);
+           else
+            $('body').append($('<div></div>').css('background-color', 'red').css('display','inline-block').attr('id', 'Komunikat')
+                   .append('otrzymano dane: '+ dane['status'] + "<br/>Tytuł: " + dane['title'] + "<br/>Tekst: " + dane['text']));
        });
    });
+
+    $("")
 });
